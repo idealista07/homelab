b2c1213d23996a600cbbb42a5bd9f20c42b8b78c
Douglas Ramos <26663633+idealista07@users.noreply.github.com>
2024-08-29 18:11:07 -0300

    Create local_define.php

---
+++ SRV-L-GLPI/local_define.php
@@ -0,0 +1,7 @@
+<?php
+define('GLPI_ROOT','/public');
+define('GLPI_VAR_DIR', '/var/glpi_data');
+define('GLPI_LOG_DIR', '/var/glpi_log');
+define('GLPI_SMTP_SERVER', 'smtp.example.com');
+define('GLPI_SMTP_PORT', 587);
+?>
